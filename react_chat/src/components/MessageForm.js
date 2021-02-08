ee2927bfb7db4c5132d05ca2d18d751b4aa9ccb5
suropark <xoopp5818@gmail.com>
2021-02-08 16:23:23 +0900

    전송 메세지 폼 구현, 이미지 전송까지 가능, antd css

---
+++ react_chat/src/components/MessageForm.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react'
+import { sendMessage, isTyping } from 'react-chat-engine'
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+function MessageForm(props) {
+    const [value, setvalue] = useState('')
+    const { chatId, creds } = props;
+
+    const handleSubmit = () => {
+       window.event.preventDefault();
+
+        const text = value.trim();
+
+        if(text.length >0 ) sendMessage(creds, chatId, { text });
+
+        setvalue('');
+    }
+
+    const handleChange = (event) => {
+        setvalue(event.target.value);
+        isTyping(props, chatId)  // 메세지 보낸 사람
+    }
+
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, { files: event.target.files, text: ''})
+    }
+
+    return (
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input
+                className="message-input"
+                placeholder="메시지를 입력하세요..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+            <label htmlFor="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon" />
+                </span>
+            </label>
+            <input type="file"
+            multiple={false}
+            id="upload-button"
+            style={{ display: 'none'}}
+            onChange={{handleUpload}}/>
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon" />
+            </button>
+            </form>
+    )
+}
+
+export default MessageForm
