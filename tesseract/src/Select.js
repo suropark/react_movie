36f2fb196c2248bec4907a3af07f8076ce109267
suroPark <xoopp5818@gmail.com>
2021-02-20 13:47:46 +0900

    빈칸 문제 출력과 정답 확인,

--- tesseract/src/Select.js
+++ tesseract/src/Select.js
@@ -6,15 +6,19 @@ function Select() {
   const [url, setUrl] = useState("");
   const [text, setText] = useState("");
   const [question, setQuestion] = useState("");
-  const [answer, setAnswer] = useState("")
+  const [answer, setAnswer] = useState("");
+  const [inputAnswer, setInputAnswer] = useState("");
+
   const clickHandler = () => {
     console.log("clicked");
     console.log(lang);
   };
-  const selectHandler = (e) => {
-    console.log("q");
-    setLang(e.target.value);
+  const selectHandler = (event) => {
+    setLang(event.target.value);
     console.log(lang);
+  };
+  const inputHandler = (event) => {
+    setInputAnswer(event.target.value);
   };
 
   const changeHandler = (event) => {
@@ -23,19 +27,27 @@ function Select() {
     console.log(typeof url);
   };
 
+  const submitHandler = () => {
+    if (answer === inputAnswer) {
+      console.log("일치");
+    } else {
+      console.log("불일치");
+    }
+  };
+
   const generateQuestion = () => {
-    let dividedText = text.split(" ");  // 1. 문자열 분리해 배열로
-    console.log(dividedText);   
+    let dividedText = text.split(" "); // 1. 문자열 분리해 배열로
+    console.log(dividedText);
     console.log(dividedText.length);
     let randomNum = Math.floor(Math.random() * dividedText.length); // 단어 개수만큼 난수 생성
     setAnswer(dividedText[randomNum]); // 한자리 빈칸의 정답
-    console.log(answer)
-    dividedText[randomNum] = "___";  // 한자리 빈칸으로 만듬
-    setQuestion(dividedText.join(" "));  // 배열을 빈칸있는 문자열로 변경 ->ㄴㅇㅌㅊㅋ 
-    console.log(question)
+    console.log(answer);
+    dividedText[randomNum] = "_____"; // 한자리 빈칸으로 만듬
+    setQuestion(dividedText.join(" ")); // 배열을 빈칸있는 문자열로 변경 ->ㄴㅇㅌㅊㅋ
+    console.log(question);
   };
 
-  const imageToWord = (e) => {
+  const imageToWord = (event) => {
     const worker = createWorker({
       logger: (m) => console.log(m),
     });
@@ -55,9 +67,8 @@ function Select() {
       await worker.terminate();
       setText(text);
     })();
-
-
   };
+
   return (
     <div>
       <h3> 언어 선택</h3>
@@ -76,15 +87,21 @@ function Select() {
         onChange={changeHandler}
       />
       <button onClick={imageToWord}>문자 변환</button>
-
-      <div style={{ marginTop: "100px", display: "flex" }}>
-        <img src={url} style={{ maxWidth: "800px" }} />
-        <span style={{ maxWidth: "800px" }}>
+      <div style={{ display: "flex" }}>
+        <span style={{ maxWidth: "40%" }}>
           <h3>{text}</h3>
         </span>
+        <span style={{ marginLeft: "20px" }}>
+          <h3>{question}</h3>
+        </span>
       </div>
-      <div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <button onClick={generateQuestion}> 문제 만들기 </button>
+        <span>
+          빈칸에 들어갈 단어 :{" "}
+          <input type="text" onChange={inputHandler} value={inputAnswer} />
+          <button onClick={submitHandler}> 제출</button>
+        </span>
       </div>
     </div>
   );
